6db80ad2279a3326d2a9e96bcb03035e37eb6fbc
pujithbhumireddy <pujithbhumireddy@gmail.com>
2024-12-03 13:15:22 +0530

    updated the files

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-fetch('navbar.html')
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    fetch('navbar.html')
         .then(response => response.text())
         .then(html => document.getElementById('navbar').innerHTML = html);
+
+})
